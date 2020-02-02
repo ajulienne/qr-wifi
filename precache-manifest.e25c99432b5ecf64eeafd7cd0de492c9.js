796e7da642ec4325ae1c31ab1ab2578d8d0add23
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-02 21:39:24 +0000

    Deploy ajulienne/qr-wifi to github.com/ajulienne/qr-wifi.git:gh-pages

---
+++ precache-manifest.e25c99432b5ecf64eeafd7cd0de492c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac58da122b59cc49ad79e237ffddb03",
+    "url": "/qr-wifi/index.html"
+  },
+  {
+    "revision": "6ac8ee296e856f73521f",
+    "url": "/qr-wifi/static/css/main.b2ebe043.chunk.css"
+  },
+  {
+    "revision": "8b00a85cb1afa1203753",
+    "url": "/qr-wifi/static/js/2.09da1562.chunk.js"
+  },
+  {
+    "revision": "04961fbb12eb46903ddef60e071916fe",
+    "url": "/qr-wifi/static/js/2.09da1562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac8ee296e856f73521f",
+    "url": "/qr-wifi/static/js/main.41be5f4f.chunk.js"
+  },
+  {
+    "revision": "5ac12fbb7965bb125555",
+    "url": "/qr-wifi/static/js/runtime-main.0844363b.js"
+  }
+]);
